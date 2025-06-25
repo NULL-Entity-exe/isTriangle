951792d4abaa8ad17ea9123a709ade67d581c0ce
NULL <162877008+NULL-Entity-exe@users.noreply.github.com>
2025-06-25 15:23:35 +0530

    isTriangle_v3.py
    added main() function and default parameter for getPoint() function

--- isTriangle_v3.py
+++ isTriangle_v3.py
@@ -2,7 +2,7 @@ import math
 
 # This function takes input from the user
 # and returns a list of floats (coordinates of a point)
-def getPoint(n):
+def getPoint(n = '\b'):
     while True:
         x = input(f"Enter the point {n} : ")
         xs = x.split()
@@ -15,9 +15,9 @@ def getPoint(n):
             except ValueError:
                 print("Please enter a valid point")
 
-def distance(p1, p2):
+def distance(x, y):
     # Calculation of length of the sides using the distance formula
-    return math.sqrt(sum((p1[i] - p2[i]) ** 2 for i in range(len(p1))))  # This sum() will iterate the (a[n] - b[n]) ^ 2 for the number of dimenstions (expantion of the dimensionality of the distance formula)
+    return math.sqrt(sum((x[i] - y[i]) ** 2 for i in range(len(x))))  # This sum() will iterate the (a[n] - b[n]) ^ 2 for the number of dimenstions (expantion of the dimensionality of the distance formula)
 
 # This function checks if the points can form a triangle
 def isTriangle(a, b, c):
@@ -28,23 +28,29 @@ def isTriangle(a, b, c):
     
     # Check if the sum of lengths of any two sides is greater than the third side i.e Triangular Inequality (a + b > c)
     return (ab + bc > ac) and (bc + ac > ab) and (ab + ac > bc)  
+    
+def main():
+    # loop to ensure all points are in the same dimension
+    while True:
 
-# loop to ensure all points are in the same dimension
-while True:
-    p1 = getPoint(1)
-    p2 = getPoint(2)
-    p3 = getPoint(3)
+        global p1, p2, p3
+        
+        p1 = getPoint(1)
+        p2 = getPoint(2)
+        p3 = getPoint(3)
 
-    if len(p1) == len(p2) and len(p2) == len(p3):   # Check if all points are in the same dimension
-        break
+        if len(p1) == len(p2) and len(p2) == len(p3):   # Check if all points are in the same dimension
+            break
+        else:
+            print("Enter all the points in the same the dimension") 
+
+    if isTriangle(p1, p2, p3):
+        print("The points can form a Triangle")
     else:
-        print("Enter all the points in the same the dimension")
+        print("The points can't form a Triangle")
 
-if isTriangle(p1, p2, p3):
-    print("The points can form a Triangle")
-else:
-    print("The points can't form a Triangle")
+    # print(f"Length of side AB: {ab} units")
+    # print(f"Length of side BC: {bc} units")   
+    # print(f"Length of side AC: {ac} units")
 
-# print(f"Length of side AB: {ab} units")
-# print(f"Length of side BC: {bc} units")   
-# print(f"Length of side AC: {ac} units")
+main()
